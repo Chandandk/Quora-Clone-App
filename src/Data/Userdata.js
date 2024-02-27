b2e025bd8dbef5676265067c0e976d2a9900dacd
Chandan Kumar <c.singh.8425@gmail.com>
2024-02-27 16:23:27 +0530

    Quora frontend React Project

---
+++ src/Data/Userdata.js
@@ -0,0 +1,53 @@
+export const Userdata = [
+    {
+      Id:1,
+      name: "Fabian Wolf",
+      logoimage:
+        "https://static.vecteezy.com/system/resources/previews/016/460/767/large_2x/linux-os-logo-top-operating-system-signs-free-png.png",
+      Question:
+        "What is mobile operating system ?",
+      Answer:
+        "A mobile operating system (OS) is essentially the software that powers smartphones, tablets, and other mobile devices. It acts as an intermediary between the device's hardware and the software applications you use, similar to how an operating system works on a computer. Here are some key points about mobile OSes:",
+      image:
+        "https://qph.cf2.quoracdn.net/main-qimg-ce518ff3fabb6cf6d06c5c906817471f-c",
+        
+    },
+    {
+      Id:2,
+      name: "Mary Ann Besser",
+      logoimage:
+        "https://upload.wikimedia.org/wikipedia/en/3/3d/Po_from_DreamWorks_Animation%27s_Kung_Fu_Panda.png",
+      Question: " Have you ever seen pandans?",
+      Answer:
+        " Years ago, I saw these pandas at the National Zoo. They’re beautiful.",
+      image:
+        " https://qph.cf2.quoracdn.net/main-qimg-240fe7756c30d7604642ff6ddf7737d0",
+      
+      },
+    {
+      Id:3,
+      name: "Praveen Kumar Rajendran",
+      logoimage:
+        "https://www.ufpb.br/ufpb/icons/redes-sociais/quora.png/@@images/image.png ",
+      Question: " Is IT industry really getting overcrowded? What is the future of IT industry in India?",
+      Answer:
+        " As per my understanding, India will be the biggest digital market in the world. We have crossed 140+ cr population. Government of India is also promoting digitalization in the full swing. As the result of such massive digital wave, the following things are happening",
+      image:
+        "https://qph.cf2.quoracdn.net/main-qimg-2000126ed2c0a14c2b26becae6d39aa7",
+       
+    },
+    {
+      Id:4,
+      name: "Fact sky ",
+      logoimage:
+        " https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHUdjrldofdIJ5w5dB7oC2rTKqdtS6fTesc7jOmPqJl2RbMzmJ4j-IIRMyrZxcZgHJhkQ&usqp=CAU ",
+      Question: "  What are some interesting facts which are not commonly known?",
+      Answer:
+        " Some interesting facts 👇👇👇",
+      image:
+        "https://qph.cf2.quoracdn.net/main-qimg-f05a2faf76899242e0fc39255b79e5da",
+         
+      },
+  ];
+
+  
---
+++ src/component/Rightbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "../componentcss/Rightbar.css";
+export default function Rightbar() {
+  return (
+    <div className="RightSide">
+    <div className="right1">
+      <img src="https://martechseries.com/wp-content/uploads/2022/10/Quora-launches-video-ads.jpg " alt=""/>
+    </div>
+    <div className="right2">
+    <img src=" https://blog.replug.io/wp-content/uploads/2022/01/How-to-Advertise-on-Quora.jpg" alt=""/>
+    </div>
+    </div>
+  )
+}
+
+ 
